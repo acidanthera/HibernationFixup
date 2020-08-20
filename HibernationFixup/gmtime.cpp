e278709b28037ae7780475105b51fd440289c1ba
lvs1974 <lvs1974@users.noreply.github.com>
2020-08-20 08:41:22 +0200

    Fix leap year detection function (thanks to PMHeart)

--- HibernationFixup/gmtime.cpp
+++ HibernationFixup/gmtime.cpp
@@ -13,7 +13,7 @@
 
 bool isLeapYear(int year)
 {
-	return ((year % 400 == 0) || (year % 100 == 0) || (year % 4 == 0));
+	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
 
 int daysInYear(int year)
